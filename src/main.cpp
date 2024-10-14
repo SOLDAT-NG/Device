4698fd8bc8353ad953543c5c078606a0c55cb0f2
reverie <vincent.lim18@outlook.fr>
2024-10-15 00:17:51 +0200

    It does basic stuff with the module at least

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define GPIO_LED 2
+#define GPIO_TX2 17 
+#define GPIO_RX2 16
+
+String handshake = "AT";
+String identifier = "AT+ID";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RX2, TX2); // 8 data bits, No parity, 1 stop bit
+
+  Serial.println("Serial2 initialized at 9600 baud.");
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial2.println(identifier);
+  Serial.println("Message sent");
+
+
+  if (Serial2.available()) {
+    String data = Serial2.readStringUntil('\n'); // Read until newline
+    Serial.print("Received: ");
+    Serial.println(data);
+  }
+
+  delay(5000);
 }
